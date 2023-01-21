a0260065bcd887a9465367a34e380f6f5400f666
Matvei <32172257+DrankRock@users.noreply.github.com>
2023-01-21 15:09:24 +0100

    feat - added mtg expansions

--- ExpansionsSetup.py
+++ ExpansionsSetup.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 import requests, re, time, os
 from bs4 import BeautifulSoup
@@ -6,13 +7,14 @@ from dataclasses import dataclass
 
 FILE = open(".expansions.txt", "w+")
 
+
 def request_block(link):
     try:
         page = requests.get(link)
-        while (page.status_code != 200):
-            if (page.status_code == 429):
+        while page.status_code != 200:
+            if page.status_code == 429:
                 print("\rIP blocked  Around 1min pause.")
-                time.sleep(5)
+                time.sleep(10)
                 page = requests.get(link)
             else:  # if page 404 or else
                 return -1
@@ -21,82 +23,47 @@ def request_block(link):
     soup = BeautifulSoup(page.content, "lxml")
     return soup
 
-
-def Magic():
-    # using mtg Fandom :
-
-    soup = request_block("https://mtg.fandom.com/wiki/Set")
-    # print(soup)
-    array = str(soup.find_all("table", class_="wikitable sortable mw-collapsible")[0])
-    lines = array.split('\n')
-    i = 0
-    output = []
-    output_iterator = 0
-    try :
-        for line in lines:
-            # print(i, line)
-            if i == 2:
-                i -= 1
-            elif i == 1:
-                if "<td>" in line and "</td>" in line :
-                    i -= 1
-                    value = re.findall(r'<td>(.*?)</td>', line)[0]
-                    # print(value)
-                    output[output_iterator].append(value)
-                    # print(" -- ", output[output_iterator])
-                    output_iterator += 1
-            elif "<i>" in line and "title=" in line:
-                name = re.findall(r'\" title=\"(.*?)\">', line)[0]
-                # print(name)
-                output.append([name])
-                i = 2
-    except Exception as exc :
-        print(exc)
-    fields = ["Name", "SET"]
-    write = csv.writer(FILE)
-    write.writerow(fields)
-    write.writerows(output)
-    # print(output)
-    # print(array)
-
-def MTG():
-    url = "https://scryfall.com/sets"
-    str_soup = str(request_block(url))
-    all_lines = str_soup.split('\n')
-    output = []
-    for line in all_lines :
-        if "<small>" in line :
-            name = line.split("<small>")[0].strip()
-            abrv = line.split("<small>")[1].replace("</small>", "")
-            output.append([name, abrv])
-    output = sorted(output)
-    for elem in output:
-        print(elem)
-    return output
-    # all_names = re.findall(r'   (.*?)</small>')
-    # print(str(soup))
-
-def Cardmarket_MTG():
+# I will be sleeping 5 between each scrapes because I'm putting too much on CM
+def MTG_scraping_cardmarket():
     soup = request_block("https://www.cardmarket.com/en/Magic/Products/Singles")
     # print(soup)
     found = str(soup.find_all("select")[1])
     all_ext = re.findall(r'<option(.*?)/option>', found)
-    output=[]
-    for elem in all_ext:
-        value=re.findall(r'value=\"(.*?)\"', elem)[0]
-        name=re.findall(r'\">(.*?)<', elem)[0]
-        output.append([name, value])
-        # print([value, name])
-    output = sorted(output)
-    print(len(output))
-    # print(found)
-    # list_found = found.split('\n')
-    for line in output :
-        print(line)
-    return output
+    all_id_list = []
+    for exp in all_ext:
+        value = re.findall(r'value=\"(.*?)\"', exp)[0]
+        name = re.findall(r'\">(.*?)<', exp)[0]
+        # name = re.sub('[^a-zA-Z0-9]+', '', name)
+        all_id_list.append([name, value])
+    the_list = []
+    # all_id_list now contanins all possible expansions value
+    for expansion in all_id_list:
+        time.sleep(5) # lil sleeping
+        # go to the expansion
+        url = "https://www.cardmarket.com/en/Magic/Products/Singles?idCategory=1&idExpansion=" + expansion[
+            1] + "&idRarity=0&perSite=20"
+        print(url)
+        soup = request_block(url)
+        expansion_title_soup = str(soup.find('title'))
+        print(expansion_title_soup)
+        try:
+            expansion_title = re.findall(r'<title>(.*?) - ', expansion_title_soup)[0]
+            # scrape the first card, doesn't matter the card
+            found = str(soup.find_all("div", class_="table-body")[0])
+            urls = re.findall(r'<a href="(.*?)"', found)
+            if len(urls) > 0:
+                card_url = "https://www.cardmarket.com" + urls[0]
+                card_soup = request_block(card_url)
+                title_1 = str(card_soup.find('title'))  # get base
+                title_2 = re.findall(r'<title>(.*?) - ', title_1)[0]  # gget only interesting part
+                title_3 = re.findall(r'\((.*?)\)', title_2)  # get all thats between parenthesis
+                title = title_3[-1]  # only keep the last
+                print("\"{}\", \"{}\", {}".format(expansion_title, title, expansion[1]))
+                the_list.append([expansion_title, title, expansion[1]])
+            else:
+                print("Expansion : {} did not contain any link".format(expansion_title))
+        except Exception as exp:
+            print("there was a problem with {}, {}".format(expansion, exp))
 
 
-
-
-print(Cardmarket_MTG())
-print(MTG())
+MTG_scraping_cardmarket()
